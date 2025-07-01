684e8e22b4350a69b883989505ded655ff791f33
Amene <layla_1380@yahoo.com>
2025-07-01 15:17:24 -0500

    feat(task-2): add Kafka consumer to handle incoming transactions

--- src/main/java/com/jpmc/midascore/controller/MyController.java
+++ src/main/java/com/jpmc/midascore/controller/MyController.java
@@ -1,21 +1,10 @@
-// package com.jpmc.midascore.controller;
-
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @RestController
-// public class MyController {
-
-//     @GetMapping("/")
-//     public String home() {
-//         return "MidasCore API is running!";
-//     }
-// }
 package com.jpmc.midascore.controller;
 
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.repository.UserRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,13 +14,25 @@ public class MyController {
     @Autowired
     private UserRecordRepository userRecordRepository;
 
-    @GetMapping("/")
-    public String home() {
-        return "MidasCore API is running!";
-    }
+    @Autowired
+    private KafkaTemplate<String, UserRecord> kafkaTemplate;
+
+    @Value("${general.kafka-topic}")
+    private String topic;
 
     @PostMapping("/")
     public UserRecord createUser(@RequestBody UserRecord userRecord) {
-        return userRecordRepository.save(userRecord);
+        
+        UserRecord savedUser = userRecordRepository.save(userRecord);
+        
+        
+        kafkaTemplate.send(topic, savedUser);
+
+        return savedUser;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "MidasCore API is running!";
     }
 }
